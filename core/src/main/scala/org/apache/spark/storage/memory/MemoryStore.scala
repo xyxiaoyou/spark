fadd1db61cc31369be6c24651407b51c84842cd1
rmishra <rmishra@pivotal.io>
2016-10-19 12:40:40 +0530

    MemoryStore changes for our changes

--- core/src/main/scala/org/apache/spark/storage/memory/MemoryStore.scala
+++ core/src/main/scala/org/apache/spark/storage/memory/MemoryStore.scala
@@ -696,6 +696,9 @@ private[storage] class PartiallyUnrolledIterator[T](
   override def next(): T = {
     if (unrolled == null) {
       rest.next()
+    } else if (!unrolled.hasNext) {
+      releaseUnrollMemory()
+      rest.next
     } else {
       unrolled.next()
     }
