0aff0d56ae73517d7f892825dfa23052074e7bb8
snappy-sachin <skapse@snappydata.io>
2016-12-16 16:22:02 +0530

    [SNAPPYDATA] Fixes for Scala Style precheckin failure. (#39)

--- core/src/main/scala/org/apache/spark/ui/UIUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/UIUtils.scala
@@ -574,9 +574,10 @@ private[spark] object UIUtils extends Logging {
     }
   }
 
-  def getProductVersionNode = {
+  def getProductVersionNode(): Unit = {
     val versionTooltipText =
-      "SnappyData Ver. " + SparkUI.getProductVersion + " ( Underlying Spark Ver. " + org.apache.spark.SPARK_VERSION + " )"
+      "SnappyData Ver. " + SparkUI.getProductVersion + " ( Underlying Spark Ver. " +
+          org.apache.spark.SPARK_VERSION + " )"
 
     <span class="version" style="font-size: 14px;" data-toggle="tooltip" data-placement="bottom"
           data-original-title={versionTooltipText} > {SparkUI.getProductVersion} </span>
