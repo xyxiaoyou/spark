f49a3ac3f7a0bdc9660b7023384383d6c1f531ef
snappy-sachin <skapse@snappydata.io>
2017-01-03 15:36:57 +0530

    SNAP-1281: UI does not show up if spark shell is run without snappydata (#42)
    Fixes: Re-enabling the default spark redirection handler to redirect user to spark jobs page.

--- core/src/main/scala/org/apache/spark/ui/SparkUI.scala
+++ core/src/main/scala/org/apache/spark/ui/SparkUI.scala
@@ -67,7 +67,7 @@ class SparkUI private (
     attachTab(new EnvironmentTab(this, store))
     attachTab(new ExecutorsTab(this))
     attachHandler(createStaticHandler(SparkUI.STATIC_RESOURCE_DIR, "/static"))
-    // attachHandler(createRedirectHandler("/", "/jobs/", basePath = basePath))
+    attachHandler(createRedirectHandler("/", "/jobs/", basePath = basePath))
     attachHandler(ApiRootResource.getServletHandler(this))
 
     // These should be POST only, but, the YARN AM proxy won't proxy POSTs
