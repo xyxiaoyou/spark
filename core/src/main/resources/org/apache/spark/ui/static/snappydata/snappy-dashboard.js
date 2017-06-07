6ce62bc044717edcba8d54a915c88446a2c488b5
snappy-sachin <skapse@snappydata.io>
2017-06-07 18:44:05 +0530

    SNAP-1698: Snappy Dashboard UI Enhancements (#55)
    * SNAP-1698: Snappy Dashboard UI Enhancements
    Changes:
      - CSS styling and JavaScript code changes for displaying Snappy cluster CPU usage widget.
      - Removed Heap and Off-Heap usage widgets.
      - Adding icons/styling for displaying drop down and pull up carets/pointers to expand cell details.
      - Adding handler for toggling expand and collapse cell details.

--- core/src/main/resources/org/apache/spark/ui/static/snappydata/snappy-dashboard.js
+++ core/src/main/resources/org/apache/spark/ui/static/snappydata/snappy-dashboard.js
@@ -1,17 +1,31 @@
 
+function toggleCellDetails(detailsId) {
+
+  $("#"+detailsId).toggle();
+
+  var spanId = $("#"+detailsId+"-btn");
+  if(spanId.hasClass("caret-downward")) {
+    spanId.addClass("caret-upward");
+    spanId.removeClass("caret-downward");
+  } else {
+    spanId.addClass("caret-downward");
+    spanId.removeClass("caret-upward");
+  }
+}
 
 function createStatusBlock() {
 
-  var avgMemoryUsage = $( "div#avgMemoryUsage" ).data( "value" );
-  var avgHeapUsageGauge = $( "div#avgHeapUsage" ).data( "value" );
-  var avgOffHeapUsageGauge = $( "div#avgOffHeapUsage" ).data( "value" );
-  var avgJVMHeapUsageGauge = $( "div#avgJvmHeapUsage" ).data( "value" );
+  var cpuUsage = $( "div#cpuUsage" ).data( "value" );
+  var memoryUsage = $( "div#memoryUsage" ).data( "value" );
+  // var heapUsageGauge = $( "div#heapUsage" ).data( "value" );
+  // var offHeapUsageGauge = $( "div#offHeapUsage" ).data( "value" );
+  var jvmHeapUsageGauge = $( "div#jvmHeapUsage" ).data( "value" );
 
   var config = liquidFillGaugeDefaultSettings();
   config.circleThickness = 0.15;
   config.circleColor = "#3EC0FF";
   config.textColor = "#3EC0FF";
-  config.waveTextColor = "#3EC0FF";
+  config.waveTextColor = "#00B0FF";
   config.waveColor = "#A0DFFF";
   config.textVertPosition = 0.8;
   config.waveAnimateTime = 1000;
@@ -23,19 +37,12 @@ function createStatusBlock() {
   config.textSize = 0.75;
   config.waveCount = 2;
 
-  var memoryGauge = loadLiquidFillGauge("memoryUsageGauge", avgMemoryUsage, config);
-  var heapGauge = loadLiquidFillGauge("heapUsageGauge", avgHeapUsageGauge, config);
-  var offHeapGauge = loadLiquidFillGauge("offHeapUsageGauge", avgOffHeapUsageGauge, config);
-  var jvmGauge = loadLiquidFillGauge("jvmHeapUsageGauge", avgJVMHeapUsageGauge, config);
+  var cpuGauge = loadLiquidFillGauge("cpuUsageGauge", cpuUsage, config);
+  var memoryGauge = loadLiquidFillGauge("memoryUsageGauge", memoryUsage, config);
+  // var heapGauge = loadLiquidFillGauge("heapUsageGauge", heapUsageGauge, config);
+  // var offHeapGauge = loadLiquidFillGauge("offHeapUsageGauge", offHeapUsageGauge, config);
+  var jvmGauge = loadLiquidFillGauge("jvmHeapUsageGauge", jvmHeapUsageGauge, config);
 
-
-  /* function NewValue(){
-      if(Math.random() > .5){
-          return Math.round(Math.random()*100);
-      } else {
-          return (Math.random()*100).toFixed(1);
-      }
-  } */
 }
 
 $(document).ready(function() {
