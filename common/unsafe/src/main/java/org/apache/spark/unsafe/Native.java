435d321e1de28161e94ce2d806029aca70d20b4f
Sumedh Wale <swale@snappydata.io>
2017-09-05 13:25:34 +0530

    [SNAP-1993] Optimize UTF8String.contains (#78)
    - Optimized version of UTF8String.contains that improves performance by 40-50%.
      However, it is still 1.5-3X slower than JDK String.contains (that probably uses JVM intrinsics
      since the library version is slower than the new UTF8String.contains)
    - Adding native JNI hooks to UTF8String.contains and ByteArrayMethods.arrayEquals if
      present.
    
    Comparison when searching in decently long strings (100-200 characters from customers.csv treating full line as a single string).
    
    Java HotSpot(TM) 64-Bit Server VM 1.8.0_144-b01 on Linux 4.10.0-33-generic
    Intel(R) Core(TM) i7-5600U CPU @ 2.60GHz
    compare contains:                        Best/Avg Time(ms)    Rate(M/s)   Per Row(ns)   Relative
    ------------------------------------------------------------------------------------------------
    UTF8String (orig)                              241 /  243          4.7         214.4       1.0X
    UTF8String (opt)                               133 /  137          8.4         118.4       1.8X
    String                                          97 /   99         11.6          86.4       2.5X
    Regex                                          267 /  278          4.2         237.5       0.9X
    
    Conflicts:
    	common/unsafe/src/main/java/org/apache/spark/unsafe/array/ByteArrayMethods.java

---
+++ common/unsafe/src/main/java/org/apache/spark/unsafe/Native.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2017 SnappyData, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
+package org.apache.spark.unsafe;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.security.CodeSource;
+import java.util.Locale;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Optimized JNI calls.
+ */
+public final class Native {
+
+  public static final int MIN_JNI_SIZE = 32;
+
+  public static final boolean debug;
+  private static final Logger logger;
+
+  private static final boolean is64Bit;
+  private static final boolean isSolaris;
+  private static final boolean nativeLoaded;
+
+  private Native() {
+  }
+
+  static {
+    debug = Boolean.getBoolean("spark.native.debug");
+
+    String arch = System.getProperty("os.arch");
+    is64Bit = arch.contains("64") || arch.contains("s390x");
+    String os = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
+    isSolaris = os.contains("sunos") || os.contains("solaris");
+
+    logger = Logger.getLogger(Native.class);
+
+    String library = isSolaris() ? "native_sol" : "native";
+    if (is64Bit()) {
+      library += "64";
+    }
+    if (debug) {
+      library += "_g";
+    }
+
+    boolean loaded = false;
+    CodeSource cs = Native.class.getProtectionDomain().getCodeSource();
+    URL jarURL = cs != null ? cs.getLocation() : null;
+    String libDir;
+    try {
+      if (jarURL != null) {
+        libDir = new File(URLDecoder.decode(jarURL.getFile(), "UTF-8"))
+            .getParentFile().getCanonicalPath();
+      } else {
+        // try in SNAPPY_HOME and SPARK_HOME
+        String productHome = System.getenv("SNAPPY_HOME");
+        if (productHome == null) {
+          productHome = System.getenv("SPARK_HOME");
+        }
+        if (productHome == null) {
+          throw new IllegalStateException("Unable to locate jar location");
+        }
+        libDir = new File(productHome, "jars").getCanonicalPath();
+      }
+      File libraryPath = new File(libDir, System.mapLibraryName(library));
+      if (libraryPath.exists()) {
+        System.load(libraryPath.getPath());
+        logger.info("library " + library + " loaded from " + libraryPath);
+      } else {
+        System.loadLibrary(library);
+        logger.info("library " + library + " loaded from system path");
+      }
+
+      loaded = true;
+    } catch (IOException ioe) {
+      if (logger.isInfoEnabled()) {
+        logger.info("library " + library + " could not be loaded due to " + ioe);
+      }
+    } catch (UnsatisfiedLinkError ule) {
+      if (logger.isInfoEnabled()) {
+        logger.info("library " + library + " could not be loaded");
+      }
+    }
+    nativeLoaded = loaded;
+  }
+
+  public static boolean is64Bit() {
+    return is64Bit;
+  }
+
+  public static boolean isSolaris() {
+    return isSolaris;
+  }
+
+  public static boolean isLoaded() {
+    return nativeLoaded;
+  }
+
+  public static native boolean arrayEquals(long leftAddress,
+      long rightAddress, long size);
+
+  public static native boolean containsString(long sourceAddress,
+      long sourceEnd, long destAddress, int destSize);
+}
