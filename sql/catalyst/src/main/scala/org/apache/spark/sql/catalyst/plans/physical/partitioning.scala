13de9dc727dc2ee535e96f2bcd5d5509171cd1f3
Sumedh Wale <swale@snappydata.io>
2017-09-16 23:00:23 +0530

    [SNAP-2033] pass the original number of buckets in table via OrderlessHashPartitioning (#82)
    also reduced parallel forks in tests to be same as number of processors/cores

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/physical/partitioning.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/physical/partitioning.scala
@@ -254,7 +254,7 @@ case object SinglePartition extends Partitioning {
  * than this partitioning then also it is considered equal.
  */
 case class OrderlessHashPartitioning(expressions: Seq[Expression],
-    aliases: Seq[Seq[Attribute]], numPartitions: Int, numBuckets: Int)
+    aliases: Seq[Seq[Attribute]], numPartitions: Int, numBuckets: Int, tableBuckets: Int)
     extends Expression with Partitioning with Unevaluable {
 
   override def children: Seq[Expression] = expressions
