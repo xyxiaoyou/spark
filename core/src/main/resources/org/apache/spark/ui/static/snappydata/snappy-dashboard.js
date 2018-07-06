6227ef38820bba58e70675718d39957b9dcc224d
snappy-sachin <skapse@snappydata.io>
2018-07-06 18:26:24 +0530

    Code changes for SNAP-2144: (#109)
    * Code changes for SNAP-2144:
     - JavaScript and CSS changes for displaying CPU cores details on Dashboard page.
     - Adding animation effect to CPU Core details.

--- core/src/main/resources/org/apache/spark/ui/static/snappydata/snappy-dashboard.js
+++ core/src/main/resources/org/apache/spark/ui/static/snappydata/snappy-dashboard.js
@@ -1,6 +1,38 @@
 
 var isMemberCellExpanded = {};
 
+function TotalCoresClickHandler() {
+  var dch = $("#DistribCoresHolder");
+  var target = $( "#CPUCoresDetails" );
+  if(dch.is(':hidden')) {
+    target.animate({
+               width: "400px"
+             }, {
+               queue: false,
+               duration: 500,
+               complete: function() {
+                 dch.show(200);
+               }
+             });
+  } else {
+    target.animate({
+               width: "125px"
+             }, {
+               queue: false,
+               duration: 500,
+               start: function() {
+                 dch.hide(200);
+               }
+             });
+  }
+}
+
+function updateCoreDetails(coresInfo) {
+  $("#totalCores").html(coresInfo.totalCores);
+  $("#locatorCores").html(coresInfo.locatorCores);
+  $("#leadsCores").html(coresInfo.leadCores);
+  $("#dataServerCores").html(coresInfo.dataServerCores);
+}
 
 function toggleCellDetails(detailsId) {
 
@@ -500,6 +532,8 @@ function loadClusterInfo() {
         extTableStatsGridCurrPage = 0;
       }
 
+      updateCoreDetails(clusterInfo.coresInfo);
+
     },
     error: ajaxRequestErrorHandler
    });
@@ -524,6 +558,8 @@ $(document).ready(function() {
   $.ajaxSetup({
       cache : false
     });
+
+  $( "#TotalCoresHolder" ).click(TotalCoresClickHandler);
 
   // Members Grid Data Table
   membersStatsGrid = $('#memberStatsGrid').DataTable( getMemberStatsGridConf() );
