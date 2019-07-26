5144ac2761d65d57c2394f913c82ba08188447c3
snappy-sachin <skapse@snappydata.io>
2019-07-27 00:06:41 +0530

    SNAP-2779 and SNAP-1338 : (#160)
    * Code changes for SNAP-2779 and SNAP-1338:
     - Adding Redundancy column in Tables List to view count of redundant copies.
     - Adding Redundancy Status column in Tables List to monitor redundancy has been satisfied or broken.
     - Display "NA" in Redundancy Status when redundancy is 0.
     - Display Redundancy as 'NA' if distribution type is REPLICATE.
     - Display buckets count in Red color, if any of the buckets is offline.

--- core/src/main/resources/org/apache/spark/ui/static/snappydata/snappy-dashboard.js
+++ core/src/main/resources/org/apache/spark/ui/static/snappydata/snappy-dashboard.js
@@ -408,9 +408,52 @@ function getTableStatsGridConf() {
       },
       { // Bucket Count
         data: function(row, type) {
-                var bcHtml = '<div style="padding-right:10px; text-align:right;">'
-                             + row.bucketCount
+                var bcHtml = '';
+                if (row.isAnyBucketLost) {
+                  bcHtml = '<div style="padding-right:10px; text-align:right; color:#ea4335;">'
+                           + row.bucketCount
+                         + '</div>';
+                } else {
+                  bcHtml = '<div style="padding-right:10px; text-align:right;">'
+                           + row.bucketCount
+                         + '</div>';
+                }
+                return bcHtml;
+              }
+      },
+      { // Redundancy
+        data: function(row, type) {
+                var bcHtml = '';
+                if (row.distributionType == "REPLICATE") {
+                  bcHtml = '<div style="padding-right:10px; text-align:right;">'
+                           + 'NA'
+                         + '</div>';
+                } else {
+                  bcHtml = '<div style="padding-right:10px; text-align:right;">'
+                           + row.redundancy
+                         + '</div>';
+                }
+                return bcHtml;
+              }
+      },
+      { // Redundancy Status
+        data: function(row, type) {
+                var bcHtml = '';
+                if (row.redundancy == 0) {
+                  bcHtml = '<div style="padding-right:10px; text-align:right;">'
+                           + 'NA'
+                         + '</div>';
+                } else {
+                  if (row.redundancyImpaired) {
+                    bcHtml = '<div style="padding-right:10px; text-align:right; color:#ea4335;">'
+                             + 'BROKEN'
                            + '</div>';
+                  } else {
+                    bcHtml = '<div style="padding-right:10px; text-align:right; color:#34a853;">'
+                             + 'SATISFIED'
+                           + '</div>';
+                  }
+                }
                 return bcHtml;
               }
       }
